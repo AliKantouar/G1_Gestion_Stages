79f916b57bbca7f3e30ca740d8a4da2d2701d0f0
ValentinCOTTE <valentin.cotte@hotmail.fr>
2017-04-24 05:21:31 +0200

    Maj page d'intro

--- src/Interface_Graphique/choix.java
+++ src/Interface_Graphique/choix.java
@@ -19,67 +19,43 @@ public class choix extends JPanel {
 	Application app;
 	
 	public choix(Application a) {
-		
+
 		this.app=a;
-		setLayout(new BorderLayout(0, 0));
 		
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{75, 0, 0, 0, 76, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
-		this.setLayout(gridBagLayout);
+		gridBagLayout.columnWidths = new int[]{60, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{63, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
 		
-		JPanel panel_1 = new JPanel();
-		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
-		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_panel_1.fill = GridBagConstraints.BOTH;
-		gbc_panel_1.gridx = 1;
-		gbc_panel_1.gridy = 0;
-		this.add(panel_1, gbc_panel_1);
-		
-		JButton btnNewButton = new JButton("USER");
-		btnNewButton.addActionListener(new AllerPage(this.app,5));
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
+		JButton btnNewButton = new JButton("Admin");
+		btnNewButton.addActionListener(new AllerPage(this.app,13));
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 16));
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridx = 2;
 		gbc_btnNewButton.gridy = 1;
-		this.add(btnNewButton, gbc_btnNewButton);
+		add(btnNewButton, gbc_btnNewButton);
 		
-		JButton btnNewButton_2 = new JButton("ENTREPRISE");
-		btnNewButton_2.addActionListener(new AllerPage(this.app,10));
-		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 15));
-		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_2.gridx = 2;
-		gbc_btnNewButton_2.gridy = 1;
-		this.add(btnNewButton_2, gbc_btnNewButton_2);
-		
-		JButton btnNewButton_1 = new JButton("ADMIN");
-		btnNewButton_1.addActionListener(new AllerPage(this.app,13));
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		JButton btnNewButton_1 = new JButton("Utilisateur");
+		btnNewButton_1.addActionListener(new AllerPage(this.app,5));
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 16));
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
 		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_1.gridx = 3;
-		gbc_btnNewButton_1.gridy = 1;
-		this.add(btnNewButton_1, gbc_btnNewButton_1);
+		gbc_btnNewButton_1.gridx = 1;
+		gbc_btnNewButton_1.gridy = 4;
+		add(btnNewButton_1, gbc_btnNewButton_1);
 		
-		JPanel panel_3 = new JPanel();
-		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
-		gbc_panel_3.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_3.fill = GridBagConstraints.BOTH;
-		gbc_panel_3.gridx = 4;
-		gbc_panel_3.gridy = 1;
-		this.add(panel_3, gbc_panel_3);
-		
-		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.insets = new Insets(0, 0, 0, 5);
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 1;
-		gbc_panel.gridy = 2;
-		this.add(panel, gbc_panel);
+		JButton btnNewButton_2 = new JButton("Entreprise");
+		btnNewButton_2.addActionListener(new AllerPage(this.app,10));
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 16));
+		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_2.gridx = 3;
+		gbc_btnNewButton_2.gridy = 4;
+		add(btnNewButton_2, gbc_btnNewButton_2);
+
 
 	}
 
